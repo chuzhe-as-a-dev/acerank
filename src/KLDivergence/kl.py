82589f63ae7f130f300dac33da582802006edd85
Faldict <legadoubledyan@gmail.com>
2017-05-09 18:36:19 +0800

    finish calculate the kl-divergence

--- src/KLDivergence/kl.py
+++ src/KLDivergence/kl.py
@@ -3,9 +3,10 @@
 from math import log
 import time
 
-authors = {}
-experts = []
-stars = []
+global authors
+global experts
+global stars
+MAXINT = 10000000.
 
 
 def preprocess(field):
@@ -26,8 +27,10 @@ def getExpert(field):
     lines = f.readlines()
     for line in lines:
         line.strip()
-        line = line[:-2]
-        experts.append(line)
+        line = line[:-1]
+        # make sure the expert is in the database
+        if line in authors:
+            experts.append(line)
     f.close()
 
 
@@ -36,47 +39,55 @@ def getStar(field):
     lines = f.readlines()
     for line in lines:
         line.strip()
-        line = line[:-2]
-        star = line.split('\t')
-        stars.append(star[0])
+        line = line[:-1]
+        star = line.split('\t')[0]
+        # make sure the star is in the database
+        if star in authors:
+            stars.append(star)
     f.close()
 
 
 def calculate_relent(p, q):
     d1 = authors[p]
     d2 = authors[q]
+    if len(d1) < 5 or len(d2) < 5:
+        return MAXINT
     map(lambda x: x / sum(d1), d1)
     map(lambda x: x / sum(d2), d2)
     ent = 0.
     for i in range(5):
         ent += d1[i] * log(d2[i] / d1[i])
-    print "%s\t%s\t%f\n" % (p, q, ent)
-    return ent
+    return abs(ent)
 
 
 def main():
-    for field in ["AI"]:
+    global authors, experts, stars
+    fields = ["AI", "Architecture", "CG", "Database", "HCI", "Network", "PL", "Security", "Theory"]
+    for field in fields:
+        # init global variables
+        authors = {}
+        experts = []
+        stars = []
         print "Start process %s at %s" % (field, time.ctime())
         preprocess(field)
         getExpert(field)
         getStar(field)
+        print "There is %d authors in %s" % (len(authors), field)
+        print "There is %d experts in %s" % (len(experts), field)
+        print "There is %d stars in %s" % (len(stars), field)
         f = open("../../result/%s_similar_author.txt" % field, "w")
         for star in stars:
-            if star not in authors:
+            sim_author = experts[0]
+            maxent = MAXINT
+            for expert in experts:
+                entropy = calculate_relent(star, expert)
+                if entropy < maxent:
+                    maxent = entropy
+                    sim_author = expert
+            # prevent the star has already become the expert
+            if star == sim_author:
                 continue
-            else:
-                maxent = 10000000.
-                sim_author = experts[0]
-                for expert in experts:
-                    if expert not in authors:
-                        print "[WARNING] There is no expert %s\n" % expert
-                        continue
-                    else:
-                        entropy = calculate_relent(star, expert)
-                        if extropy < maxent:
-                            maxent = extropy
-                            sim_author = expert
-            f.write("%s\t%s\t%f\n" % (star, expert, maxent))
+            f.write("%s\t%s\t%f\n" % (star, sim_author, maxent))
         f.close()
 
 
